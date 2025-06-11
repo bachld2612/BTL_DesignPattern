7b9312be307d4fcfecfa0ca5b298f5ee048889fa
sanghandsome <nguyendanhsanghandsome@gmail.com>
2025-06-11 21:09:41 +0700

    Observer Event

---
+++ src/main/java/com/bach/model/Customer.java
@@ -0,0 +1,25 @@
+package com.bach.model;
+
+public class Customer {
+    private int id;
+    private String fullName;
+    private String phone;
+
+    public Customer(int id, String fullName, String phone) {
+        this.id = id;
+        this.fullName = fullName;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
